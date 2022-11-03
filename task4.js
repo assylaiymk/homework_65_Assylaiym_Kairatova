0c163aa58af00e51ec2d057fb2223fcf66a16a4d
Assylaiym Kairatova <assylaiym.kairatova@gmail.com>
2022-11-03 23:55:56 +0600

    Task 4

---
+++ task4.js
@@ -0,0 +1,9 @@
+let duck = {
+    name: "Duck",
+    color: "white",
+    age: 1
+};
+var phrase = "кря, кря"
+const str = duck.toString()
+document.write("Результат в первом сообщении: ", duck,)
+document.write("Результат во втором сообщении: " + phrase)
